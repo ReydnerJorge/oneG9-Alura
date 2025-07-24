4b3ba5015552a2c443be99bba3fff21c7d8a2493
Reydner96 <reydneroliveirajorge96@gmail.com>
2025-07-24 20:38:09 -0300

    feat:  atualização no app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('Boas vindas ao jogo do número secreto!')
+
+
+let secretNumber = 5;
+
+let guess = promtp('Escolha um número entre 1 e 10: ');
+
+if (secretNumber == guess) {
+    console.log('Perfeito você acertou o número')
+};
